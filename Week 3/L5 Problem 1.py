5c2994b5d7a90668db6658565b869e8f84f8505c
Krypton410 <maximusarellius0923@gmail.com>
2017-04-25 00:43:53 +0800

    Progress 2

---
+++ Week 3/L5 Problem 1.py
@@ -0,0 +1,14 @@
+def iterPower(base, exp):
+    '''
+    base: int or float.
+    exp: int >= 0
+ 
+    returns: int or float, base^exp
+    '''
+    result = 1
+    for i in range(1,exp + 1):
+        result *= base
+
+    return round(result,4)
+    
+print iterPower(2,5)
