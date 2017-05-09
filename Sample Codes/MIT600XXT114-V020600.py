77ac92f62f08eeccaed5233ec51d3c724b3e15c3
Krypton410 <maximusarellius0923@gmail.com>
2017-05-09 12:39:12 +0800

    Sample Codes

---
+++ Sample Codes/MIT600XXT114-V020600.py
@@ -0,0 +1,12 @@
+class binaryTree(object):
+    def __init__(self, value):
+        self.value = value
+        self.leftBranch = None
+        self.leftBranch = None
+        self.parent = None
+    def setLeftBranch(self, node):
+        self.leftBranch = node
+    def setRightBranch(self, node):
+        self.leftBranch = node
+    def setParent(self, parent):
+        
